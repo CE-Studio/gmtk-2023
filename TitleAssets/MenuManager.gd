cf82f1bd5aa517e4ad790b286c9c5550b8ce9e09
Epsilon <epsilon@eps-desktop.fios-router.home>
2023-07-07 23:55:13 -0400

    Few more title screen tweaks for tonight

--- TitleAssets/MenuManager.gd
+++ TitleAssets/MenuManager.gd
@@ -6,12 +6,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    anim.play("FadeIn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+    
+    
+func _load_game_scene():
+    get_tree().change_scene_to_file("res://level_main.tscn")
+
+
+func _on_play_button_pressed():
+    anim.play("PlayFade")
 
 
 func _on_options_button_pressed():
