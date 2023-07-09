0f36b9c2f7a961585aae991f9e04743acc8ab3cf
clarence112 <clarencepacelli@gmail.com>
2023-07-09 10:52:34 -0400

    no message

---
+++ LevelAssets/laser.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+
+const APATH = "res://game components/laser.tscn"
+var on = preload("res://LevelAssets/LaserOn.svg")
+var off = preload("res://LevelAssets/LaserOff.svg")
+var part = preload("res://LevelAssets/deathpart.tscn")
+
+
+func switch(state:bool):
+    if state:
+        $AnimatableBody2D.process_mode = Node.PROCESS_MODE_INHERIT
+        texture = on
+    else:
+        $AnimatableBody2D.process_mode = Node.PROCESS_MODE_DISABLED
+        texture = off
+
+
+func _on_area_2d_body_entered(body):
+    if body is Player:
+        $"/root/Node2D/Node2D/RigidBody2D2".lost()
+        var h = part.instantiate()
+        h.position = body.position
+        $"../../".add_child(h)
+        body.queue_free()
