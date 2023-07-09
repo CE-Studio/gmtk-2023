f8e7707457e298cda10efdf088cd12021c179fff
Epsilon <epsilon@eps-desktop.fios-router.home>
2023-07-09 00:41:22 -0400

    Stuff

--- levelrun.gd
+++ levelrun.gd
@@ -7,7 +7,7 @@ var cycle := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    build("res://levels/testlevel.json")
+    build("res://levels/groundFloor.json")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
