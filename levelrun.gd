746b14231adbcabef414593dc089760805863d9a
clarence112 <clarencepacelli@gmail.com>
2023-07-08 23:15:45 -0400

    no message

--- levelrun.gd
+++ levelrun.gd
@@ -1,9 +1,13 @@
 extends RigidBody2D
 
 
+var lname := ""
+var cycle := 0.0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    build("res://levels/testlevel.json")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +18,29 @@ func _process(delta):
         apply_torque(Input.get_axis("game_move_right", "game_move_left") * delta * 8)
     else:
         apply_torque(Input.get_axis("game_move_left", "game_move_right") * delta * 8)
+        
+        
+func build(level:String) -> void:
+    for i in $content.get_children():
+        i.queue_free()
+    for i in $"../players".get_children():
+        i.queue_free()
+    var f = FileAccess.open(level, FileAccess.READ)
+    var t = f.get_as_text()
+    f.close()
+    var thedata = JSON.parse_string(t)
+    lname = thedata["name"]
+    cycle = thedata["cycle"]
+    var v2r := PackedVector2Array()
+    for i in thedata["geom"]:
+        v2r.append(Vector2(i[0], i[1]))
+    $Sprite2D/Polygon2D.polygon = v2r
+    $CollisionPolygon2D.polygon = v2r
+    for c in thedata["items"]:
+        var h:Node2D = load(c["type"]).instantiate()
+        if c["type"] == "res://game components/player.tscn":
+            $"../players".add_child(h)
+        else:
+            $content.add_child(h)
+        h.position = Vector2(c["pos"][0], c["pos"][1])
+        h.rotation = c["rot"]
